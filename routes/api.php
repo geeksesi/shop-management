1d54b8be2cee1eaef19a389a707a43c51dfa65cd
semjz <smalakjan@yahoo.com>
2023-01-18 23:52:52 +0330

    #3 Implement rate limiting with sleep

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/user/register', [App\Http\Controllers\API\UserController::class, 'register'])->name('user.register');
 
-Route::post('user/login', [App\Http\Controllers\API\UserController::class, 'login'])->name('user.login');
+Route::post('user/login', [App\Http\Controllers\API\UserController::class, 'login'])->name('user.login')
+    ->middleware('throttle:api');
