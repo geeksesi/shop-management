633be18cdbb0b904a38cf14ce3c3d3314274dbbf
semjz <smalakjan@yahoo.com>
2023-01-18 23:52:52 +0330

    #3 Implement rate limiting with sleep

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,7 +46,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return (new Limit('login', 5, 30))->by($request->user()?->id ?: $request->ip());
         });
     }
 }
