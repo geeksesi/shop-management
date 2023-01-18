42a224e09cd6907303f69c67fb1d90cc70a0d9a0
semjz <smalakjan@yahoo.com>
2023-01-18 21:23:30 +0330

    #3 Create Login method in UserController

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\UserController\LoginRequest;
 use App\Http\Requests\API\UserController\RegisterRequest;
 use App\Http\Resources\AuthenticationResource;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -19,5 +21,23 @@ class UserController extends Controller
         $token = $user->createToken('api_token');
 
         return AuthenticationResource::make($token);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::where("username", $data["username"])->get();
+
+        # empty($user) doesn't work!
+        if (count($user) == 0){
+            return response()->json("user doesn't exist.", 404);
+        }
+        else if(password_verify($data["password"], $user[0]["password"])){
+            return response()->json($user);
+        }
+        else{
+            return response()->json("Password Incorrect!", 401);
+        }
+
     }
 }
