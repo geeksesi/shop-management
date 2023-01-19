6453d56772037bd65fad1cded3c5bce35a7441e6
semjz <smalakjan@yahoo.com>
2023-01-19 12:52:38 +0330

    #3 Use UserFactory in tests

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ class UserFactory extends Factory
             'family' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => 'password', // password
             'remember_token' => Str::random(10),
             'username' => fake()->userName(),
             'phone_number' => fake()->phoneNumber(),
@@ -40,4 +40,5 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
 }
