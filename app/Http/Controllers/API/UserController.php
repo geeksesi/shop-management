b8862923795606e5c592f6815bff4ea865f05046
semjz <smalakjan@yahoo.com>
2023-01-19 14:52:30 +0330

    #3 Use LoginRequest in login method

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\API\UserController\LoginRequest;
 use App\Http\Requests\API\UserController\RegisterRequest;
 use App\Http\Resources\AuthenticationResource;
 use App\Models\User;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -34,7 +35,7 @@ class UserController extends Controller
             return AuthenticationResource::make($token);
         }
 
-        return response()->json()->setStatusCode(404);
+        throw new AuthenticationException();
 
     }
 }
