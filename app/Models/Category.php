82298f92c0a7d5896e5fba6c3355f6921cabe657
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2023-01-29 13:30:10 +0330

    #17 fix a little

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,26 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
     protected $fillable = [
-        "title" ,
-        "description" ,
-        "parent_id" ,
+        "title",
+        "description",
+        "parent_id",
     ];
 
-    #Relations
-    #TODO Relation with Product (After the product model is made)
     public function parent(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
-        return $this->belongsTo(Category::class , "parent_id");
+        return $this->belongsTo(Category::class, "parent_id");
     }
+
     public function children(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
-        return $this->hasMany(Category::class , "parent_id");
+        return $this->hasMany(Category::class, "parent_id");
     }
+
     public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(User::class);
     }
+
     public function products()
     {
         return $this->hasMany(Product::class);
