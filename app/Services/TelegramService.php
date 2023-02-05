60dc4d9745628829d70a6ee9897922a8dbd21b7c
semjz <smalakjan@yahoo.com>
2023-02-05 21:53:03 +0330

    #47 Refactor TelegramService to use laravel HTTP client

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -2,98 +2,73 @@
 
 namespace App\Services;
 
-use Exception;
+use Illuminate\Support\Facades\Http;
+
 
 class TelegramService
 {
-    private string $url;
+    private $token;
+
 
     public function __construct()
     {
-        $this->url = 'https://api.telegram.org/bot' . env("TELEGRAM_BOT_TOKEN") . '/';
+        $this->token = env("TELEGRAM_BOT_TOKEN");
     }
 
     /**
-     * @param string $_method
-     * @param array $_parameters
-     * @param string $_content_type
-     * @return array|bool
+     * @param string $method
+     * @param array $params
+     * @param string $content_type
+     * @return array
      */
-    public function execute(string $_method, array $_parameters, string $_content_type): array|bool
+    public function execute(string $method, array $params, string $content_type)
     {
-        if (!isset($this->url)) {
-            $this->__construct();
+        $url = sprintf('https://api.telegram.org/bot%s/%s', $this->token, $method);
+        $response = "";
+        if($content_type == 'multipart/form-data'){
+            $response = Http::asMultipart()->connectTimeout(30)->post($url, $params);
+        }
+        elseif ($content_type == 'application/json'){
+            $response = Http::connectTimeout(30)->post($url, $params);
         }
 
-        $url = $this->url . $_method;
-
-        $curl = curl_init($url);
-        if (empty($_parameters)) {
-            printf("Parameters are empty");
-            return false;
-        }
-
-        if ($_content_type == "application/json") {
-            $_parameters = json_encode($_parameters);
-        }
-
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $_parameters);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type:' . $_content_type]);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-        $result = curl_exec($curl);
-
-        if (curl_errno($curl)) {
-            printf(curl_error($curl));
-            return false;
-        }
-        curl_close($curl);
-
-        $result = json_decode($result, true);
-
-        if (is_null($result)) {
-            printf("output is empty!");
-            return false;
-        }
-        return $result;
+        return ['result'=>$response->json('result', []), 'status_code'=>$response->status()];
     }
 
     /**
-     * @param mixed $_photo_path
-     * @param string $_chat_id
-     * @param string $_title
-     * @param string $_description
+     * @param mixed $photo_path
+     * @param string $chat_id
+     * @param string $title
+     * @param string $description
      * @return array|bool
      */
-    public function send_photo(mixed $_photo_path, string $_chat_id, string $_title, string $_description):array|bool
+    public function send_photo(mixed $photo_path, string $chat_id, string $title, string $description):array|bool
     {
         $parameters = [
-            "photo" => $_photo_path,
-            "chat_id" => $_chat_id,
-            "caption" => sprintf("%s: \n %s", $_title, $_description)
+            "photo" => $photo_path,
+            "chat_id" => $chat_id,
+            "caption" => sprintf("%s: \n %s", $title, $description)
         ];
-
         return $this->execute('sendPhoto', $parameters, "application/json");
-
     }
 
     /**
-     * @param string $_photo_path
-     * @param string $_chat_id
-     * @param string $_title
-     * @param string $_description
+     * @param string $photo_path
+     * @param string $chat_id
+     * @param string $title
+     * @param string $description
      * @return array|bool
      */
-    public function send_photo_from_file(string $_photo_path, string $_chat_id,string $_title, string $_description):array|bool
+    public function send_photo_from_file(string $photo_path, string $chat_id,string $title, string $description):array|bool
     {
+        $photo = fopen($photo_path, 'r');
         $parameters = [
-            "photo" => curl_file_create($_photo_path),
-            "chat_id" => $_chat_id,
-            "caption" => sprintf("%s: \n %s", $_title, $_description)
+            "photo" => $photo,
+            "chat_id" => $chat_id,
+            "caption" => sprintf("%s: \n %s", $title, $description)
         ];
 
         return $this->execute('sendPhoto', $parameters, "multipart/form-data");
 
     }
-
 }
