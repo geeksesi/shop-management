47a0dc31b953a565200889a2e043fbea4fbb4f96
mj999666 <mjeelm@gmail.com>
2023-02-05 14:10:04 +0330

    #17 Created abstract class AbstractFilter

--- app/Filters/AbstractFilter.php
+++ app/Filters/AbstractFilter.php
@@ -16,14 +16,18 @@ abstract class AbstractFilter
     }
     protected function getFilters()
     {
-
+        return array_filter($this->request->only(array_keys($this->filters)));
     }
     public function filter(Builder $builder)
     {
-
+        foreach($this->getFilters() as $filter => $value)
+        {
+            $this->resolveFilter($filter)->filter($builder, $value , $this->request);
+        }
+        return $builder;
     }
     protected function resolveFilter($filter)
     {
-
+        return new $this->filters[$filter];
     }
 }
