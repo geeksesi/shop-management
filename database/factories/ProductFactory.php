d220322192319897fd70975fe0b88c73ca5c734a
semjz <smalakjan@yahoo.com>
2023-02-07 17:47:26 +0330

    #47 Fake Storage and upload file for testing purposes

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,7 +6,9 @@ use App\Enums\ProductTypeEnum;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
@@ -22,13 +24,14 @@ class ProductFactory extends Factory
     protected $model = Product::class;
     public function definition()
     {
+        Storage::fake('photos');
         return [
             'name' => fake()->name,
             'description' => fake()->text,
             'quantity' => 10,
             'price' => 2000,
             'type' => ProductTypeEnum::DIGITAL,
-            'thumbnail' => fake()->url
+            'thumbnail' => UploadedFile::fake()->create('image.jpg')
         ];
     }
 }
