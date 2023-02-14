ed88372879949b6591046f294360c2a1442b695f
NajiProgrammer <s.dehghan2016@gmail.com>
2023-02-14 12:42:38 +0330

    #12 write a test for BlogPost

---
+++ tests/Unit/Model/BlogPostTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\BlogPost;
+use App\Models\BlogComment;
+
+
+class BlogPostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function it_has_many_comments()
+    {
+        $post = BlogPost::factory()->create();
+        $comment1 = BlogComment::factory()->create(['id' => $post->id]);
+        $comment2 = BlogComment::factory()->create(['id' => $post->id]);
+
+        $this->assertInstanceOf(BlogComment::class, $post->comments()->first());
+        $this->assertEquals(2, $post->comments()->count());
+    }
+
+}
